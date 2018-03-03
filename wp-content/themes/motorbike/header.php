61df902da0fe88fb2b2e183a386f1db9ae2ed89b
certong <tongtong0507@live.com>
2018-03-03 16:51:02 +0800

    Commit for ACF Pro

--- wp-content/themes/motorbike/header.php
+++ wp-content/themes/motorbike/header.php
@@ -28,8 +28,6 @@
 
 	<header id="masthead" class="site-header" role="banner">
 
-		<?php get_template_part( 'template-parts/header/header', 'image' ); ?>
-
 		<?php if ( has_nav_menu( 'top' ) ) : ?>
 			<div class="navigation-top">
 				<div class="wrap">
@@ -37,6 +35,8 @@
 				</div><!-- .wrap -->
 			</div><!-- .navigation-top -->
 		<?php endif; ?>
+		
+		<?php get_template_part( 'template-parts/header/header', 'image' ); ?>
 
 	</header><!-- #masthead -->
 
